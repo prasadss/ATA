a2b3a1e0cb1d7b2077c693b992c07c80f9371d2d
Prasad Saitwal <psaitwal@Administrators-MacBook-Pro.local>
2020-11-22 12:09:17 +0530

    Razor pay adddd

--- src/app/components/plans/plans.component.ts
+++ src/app/components/plans/plans.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { WindowRefService } from '../../window-ref.service';
 
 @Component({
   selector: 'app-plans',
@@ -6,8 +7,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./plans.component.scss'],
 })
 export class PlansComponent implements OnInit {
-  constructor() {}
+  constructor(private winRef: WindowRefService) {}
   price = 3999;
   selected = 1;
-  ngOnInit(): void {}
+  ngOnInit(): void {
+  }
+
+  payWithRazor() {
+    const options: any = {
+      key: 'rzp_live_fuxbb9xUfXfYgj',
+      amount: this.price*100, // amount should be in paise format to display Rs 1255 without decimal point
+      currency: 'INR',
+      name: '', // company name or product name
+      description: '',  // product description
+      image: './assets/img/logo.png', // company logo or product image
+       // order_id: val, // order_id created by you in backend
+      modal: {
+        // We should prevent closing of the form when esc key is pressed.
+        escape: false,
+      },
+      notes: {
+        // include notes if any
+      },
+      theme: {
+        color: "rgb(105, 189, 81);"
+      }
+    };
+    options.handler = ((response, error) => {
+      options.response = response;
+      console.log(response);
+      console.log(options);
+      // call your backend api to verify payment signature & capture transaction
+    });
+    options.modal.ondismiss = (() => {
+      // handle the case when user closes the form while transaction is in progress
+      console.log('Transaction cancelled.');
+    });
+    debugger;
+    const rzp = new this.winRef.nativeWindow.Razorpay(options);
+    rzp.open();
+  }
+
 }
